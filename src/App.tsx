03b4c0971e7fac52ec3f640d3c29145faec376a9
fernangb <gfsouza@poli.ufrj.br>
2021-02-17 23:48:20 -0300

    Ajustes na autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
 import 'react-native-gesture-handler';
-
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import Routes from './routes/auth.routes';
+import {View, StatusBar} from 'react-native';
+import Routes from './routes';
+import {NavigationContainer} from '@react-navigation/native';
+
 import AppProvider from './hooks';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#222680" translucent />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#fff' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#222680" />
+      <AppProvider>
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
