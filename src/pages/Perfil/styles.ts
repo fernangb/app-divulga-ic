e0e74af06410d6f924d5c397ead82209439b0bb6
fernangb <gfsouza@poli.ufrj.br>
2021-03-10 14:49:27 -0300

    Atualização de perfil

--- src/pages/Perfil/styles.ts
+++ src/pages/Perfil/styles.ts
@@ -3,23 +3,31 @@ import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
   justify-content: center;
   padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
+  position: relative;
 `;
 
 export const Title = styled.Text`
-  font-size: 24px;
+  font-size: 36px;
   color: #f76769;
   font-family: 'BubblegumSans-Regular';
-  margin: 24px 0;
+  margin: 24px 0 24px;
 `;
 
 export const UserAvatar = styled.Image`
-  width: 186px;
-  height: 186px;
-  border-radius: 98px;
-  margin-top: 64px;
+  width: 164px;
+  height: 164px;
+  border-radius: 82px;
   align-self: center;
 `;
+export const UserAvatarButton = styled.TouchableOpacity`
+  margin-top: 124px;
+`;
 
-export const UserAvatarButton = styled.TouchableOpacity``;
+export const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 24px;
+  top: 24px;
+`;
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://192.168.0.13:3333',
-  baseURL: 'http://358eabb1eae0.ngrok.io',
+  baseURL: 'http://044f5f4a2f2f.ngrok.io',
 });
 
 export default api;
